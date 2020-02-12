9586f2e21b5982cd30044932695aeff4596882b1
Pavel <59092937+Pavel-Gorokhovskii@users.noreply.github.com>
2020-02-12 23:08:02 +0300

    chat with ban

--- ekran.php
+++ ekran.php
@@ -15,7 +15,7 @@
     <?php
     $mes_arr = file("text.txt");
     foreach ($mes_arr as $key => $value) {
-        $buf = explode(':', $value);
+        $buf = explode('| ', $value);
 
 
         echo "<div class = '" . (($key % 2) ? 'odd' : 'even') . "'> $buf[2] : $buf[3] </div>";
--- message.php
+++ message.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
     <title>Document</title>
 
 </head>
@@ -12,16 +13,21 @@
 <body>
     <form action='?' method='POST'>
         <input type='text' value="<?= isset($_POST['userName']) ? $_POST['userName'] : ''; ?>" name='userName'>
-        <input type='text' name='for'>
+        <input type='text' name='message'>
         <input class="submit" type="submit" value="OK">
     </form>
     <?php
 
-    if ($_SERVER['REMOTE_ADDR'] != in_array('164.177.200.208', $_SERVER) && !empty($_POST['for'])) {
-        file_put_contents('text.txt', $_SERVER['HTTP_USER_AGENT'] . ": " . $_SERVER['REMOTE_ADDR'] . ": " . $_POST['userName'] . ": " . $_POST['for'] . "\n", FILE_APPEND);
+    $banList = file('ban.txt');
+
+    if (in_array($value, $banList) == in_array('REMOTE_ADDR', $_SERVER)) {
+        echo "<div class = 'BAN'> Вас забанили </div>";
     } else {
-        echo "<div class = 'BAN' Вас забанили </div>";
+
+        file_put_contents('text.txt', $_SERVER['HTTP_USER_AGENT'] . "| " . $_SERVER['REMOTE_ADDR'] . "| " . $_POST['userName'] . "| " . $_POST['message'] . "\n", FILE_APPEND);
     }
+
+
     ?>
 
 
